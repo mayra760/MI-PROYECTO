8c23525bc4ca27a2b8a930cbb24e6571f3a27733
mayra760 <168737852+mayra760@users.noreply.github.com>
2024-09-12 11:28:57 -0500

    Add files via upload

--- instalador/fw.sql
+++ instalador/fw.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `fw` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `fw`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: fw
@@ -514,4 +516,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-10 19:49:55
+-- Dump completed on 2024-09-12 11:28:09

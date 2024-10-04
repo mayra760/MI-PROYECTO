9361ac6066e6d88ba4df5767fc925d7122b546b7
mayra760 <168737852+mayra760@users.noreply.github.com>
2024-10-04 06:12:02 -0500

    Add files via upload

--- usuarios/ctroUser.php
+++ usuarios/ctroUser.php
@@ -138,9 +138,5 @@ if (isset($_POST['cambiarfoto']) && $_POST['cambiarfoto'] === 'true') {
     } else {
         echo 'Error en la carga del archivo';
     }
-} else {
-    echo 'Solicitud no válida';
 }
-
-
 ?>

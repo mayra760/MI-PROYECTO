97d32462c4dec631f8d9d9f0d8f31aa6e6ecf48e
mayra760 <168737852+mayra760@users.noreply.github.com>
2024-10-04 07:52:11 -0500

    Add files via upload

--- method/correo_class.php
+++ method/correo_class.php
@@ -21,8 +21,8 @@ class Correo {
             $mail->SMTPDebug  =  0;                                                 //Enable verbose debug output                           
             $mail->Host       = 'smtp.mailersend.net';                              //Set the SMTP server to send through
             $mail->SMTPAuth   =  true;                                              //Enable SMTP authentication
-            $mail->Username   = 'MS_fb7crg@trial-z3m5jgr0dnmgdpyo.mlsender.net';    //SMTP username
-            $mail->Password   = 'wkHBV0rRy2nRiaY7';                                 //SMTP password
+            $mail->Username   = 'MS_XDNmBo@trial-351ndgw2yqdgzqx8.mlsender.net';    //SMTP username
+            $mail->Password   = 'A3Z6632W3UQ2pUHe';                                 //SMTP password
             $mail->SMTPSecure = 'tls';                                              //Enable implicit TLS encryption
             $mail->Port       =  587;                                               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 

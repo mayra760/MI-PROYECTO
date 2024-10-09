62dc218c24787ccfab270c77dfc7b00897143c06
mayra760 <168737852+mayra760@users.noreply.github.com>
2024-10-08 23:55:22 -0500

    Add files via upload

--- usuarios/conBaBus.php
+++ usuarios/conBaBus.php
@@ -4,9 +4,9 @@ include_once("../method/productos_class.php");
 
 if (!isset($_SESSION)) session_start();
 if (!isset($_SESSION['id'])) {
-    header("location: ../index.php");
+    header("location: ../login.php");
 } else if ($_SESSION['id'] == 0) {
-    header("location: ../index.php");
+    header("location: ../login.php");
 }
 
 $seccion = "home";
@@ -17,7 +17,7 @@ if (isset($_GET['seccion'])) {
 if ($seccion == "cerrarSe") {
     session_destroy();
     setcookie(session_name(), "", time() - 3600, "/");
-    header("location:../index.php");
+    header("location:../login.php");
 }
 
 include("navUser.php"); // Este debe estar solo aquí
